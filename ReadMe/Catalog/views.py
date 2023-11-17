831cadd1c563cafd2d54440faed372c7f3885094
Marsel <buzzakka6@gmail.com>
2023-11-17 16:48:33 +0300

    Изменил страницы объектов (автор, книги)

--- ReadMe/Catalog/views.py
+++ ReadMe/Catalog/views.py
@@ -18,7 +18,19 @@ def index(request):
 
 class BookListView(generic.ListView):
     model = Book
+    template_name = 'catalog/books/book_list.html'
 
 
 class BookDetailView(generic.DetailView):
     model = Book
+    template_name = 'catalog/books/book_detail.html'
+
+
+class AuthorListView(generic.ListView):
+    model = Author
+    template_name = 'catalog/authors/author_list.html'
+
+
+class AuthorDetailView(generic.DetailView):
+    model = Author
+    template_name = 'catalog/authors/author_detail.html'

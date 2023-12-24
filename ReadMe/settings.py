99703e42f753d1005232b4ebf135d513719ae17b
buzzakka <buzzakka6@gmail.com>
2023-12-24 10:55:08 +0000

    deploy

--- ReadMe/settings.py
+++ ReadMe/settings.py
@@ -12,11 +12,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-@$bn3)!z1nl_otb$dats%^$=w_9utz_m8z1%6m@^8jxp!nssjb'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['127.0.0.1', 'testserver']
-
-INTERNAL_IPS = ["127.0.0.1"]
+ALLOWED_HOSTS = ['*',]
 
 # Application definition
 
@@ -72,14 +70,21 @@ WSGI_APPLICATION = 'ReadMe.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+#DATABASES = {
+#    'default': {
+#        'ENGINE': 'django.db.backends.postgresql',
+#        'NAME': 'readme_bd',
+#        'USER': 'admin',
+#        'PASSWORD': 'admin',
+#        'HOST': 'localhost',
+#        'PORT': '5432',
+#    }
+#}
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'readme_bd',
-        'USER': 'admin',
-        'PASSWORD': 'admin',
-        'HOST': 'localhost',
-        'PORT': '5432',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
